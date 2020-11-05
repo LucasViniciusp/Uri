54b0e966a905b1b7b631f6cb295b00af49248d3e
Lucas Vinicius <63478557+LucasViniciusp@users.noreply.github.com>
2020-11-05 11:16:23 -0300

    Update 1250.py

--- 1250.py
+++ 1250.py
@@ -1,21 +1,28 @@
+def contar_acertos_tiros(altura_tiros, kiloman_posicao):
+    acertos = 0
+    for i in range(len(altura_tiros)):
+        altura = altura_tiros[i]
+        kiloman = kiloman_posicao[i]
+
+        if altura <= 2 and kiloman == 'S':
+            acertos += 1
+
+        elif altura > 2 and kiloman == 'J':
+            acertos += 1
+    return acertos
+
 def main():
-  qtd_testes = int(input())
+    qtd_testes = int(input())
 
-  while n > 0:
-      tiros = int(input())
-      acertos = 0
-      height = input().split()
-      kiloman = input()
+    while qtd_testes > 0:
+        tiros = int(input())
+        alturas = list(map(int, input().split()))
+        kiloman = input()
+    
+        acertos = contar_acertos_tiros(alturas, kiloman)
 
-      for i in range(len(height)):
-          if int(height[i]) <= 2 and kiloman[i] == 'S':
-              acertos += 1
-
-          elif int(height[i]) > 2 and kiloman[i] == 'J':
-              acertos += 1
-
-      print(acertos)
-      qtd_testes -= 1
+        print(acertos)
+        qtd_testes -= 1
 
 if __name__ == '__main__':
   main()
